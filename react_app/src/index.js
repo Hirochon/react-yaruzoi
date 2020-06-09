5d395c77978ac4a482abb91ae22bb001b16f36bf
Hirochon <heacet43@gmail.com>
2020-06-09 11:45:58 +0900

    [add] storejsを作成

--- react_app/src/index.js
+++ react_app/src/index.js
@@ -1,41 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
+import MemoStore from './memo/Store'
 
-
-// ステートの値
-let state_values = {
-	counter: 0,
-	message: "COUNTER"
-}
-
-// レデューサー
-function counter(state = state_values, action) {
-	switch (action.type){
-		case 'INCREMENT':
-			return {
-				counter: state.counter + 1,
-				message: "INCREMENT"
-			};
-		case 'DECREMENT':
-			return {
-				counter: state.counter - 1,
-				message: "DECREMENT"
-			};
-		default:
-			return state;
-	}
-}
-
-// ストアの作成
-let store = createStore(counter);
 
 // 表示をレンダリング
 ReactDOM.render(
-	<Provider store={store}>
+	<Provider store={MemoStore}>
 		<App />
 	</Provider>,
 	document.getElementById('root')

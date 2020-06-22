3eb46af2288a6b625c3f0a0ddb10500cb43429fe
Hirochon <heacet43@gmail.com>
2020-06-23 02:27:54 +0900

    [add] メモアプリにを永続化を追加

--- react_app/src/index.js
+++ react_app/src/index.js
@@ -7,49 +7,23 @@ import storage from 'redux-persist/lib/storage';
 import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
 import App from './App';
+import {memoReducer} from './memo/Store';
 
-
-// ステートの値
-let state_value = {
-	counter: 0,
-	message: "COUNTER"
-}
-
-// レデューサー
-function counter(state=state_value, action){
-	switch (action.type){
-		case 'INCREMENT':
-			return {
-				counter: state.counter + 1,
-				message: 'INCREMENT'
-			};
-		case 'DECREMENT':
-			return {
-				counter: state.counter - 1,
-				message: 'DECREMENT'
-			};
-		case 'RESET':
-			return {
-				counter: 0,
-				message: 'RESET'
-			};
-		default:
-			return state;
-	}
-}
 
 // Redux Persistの設定
 const persistConfig = {
-	key: 'root',
-	storage
+	key: 'memo',
+	storage,
+	blacklist: ['message', 'mode', 'fdata'],
+	whitelist: ['data']
 }
 
-// パーシストレデューサーの作成
-const persistedReducer = persistReducer(persistConfig, counter)
+// パージストレデューサーの作成
+const persistedReducer = persistReducer(persistConfig, memoReducer);
 
 // ストア、パーシスターの作成
-let store = createStore(persistedReducer)
-let pstore = persistStore(store)
+let store = createStore(persistedReducer);
+let pstore = persistStore(store);
 
 // 表示をレンダリング
 ReactDOM.render(
@@ -59,4 +33,6 @@ ReactDOM.render(
 		</PersistGate>
 	</Provider>,
 	document.getElementById('root')
-);
+)
+
+export default pstore;
